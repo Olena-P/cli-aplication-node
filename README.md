3425bd3c06d6f1829cafb951ff72c0059588f321
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2021-09-13 02:19:04 +0300

    cli finish

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # cli-aplication-node
+
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+node index.js --action list
+
+https://ibb.co/ggsZ6fP
+
+# Получаем контакт по id
+
+node index.js --action get --id 5
+
+https://ibb.co/SRqRSJ2
+
+# Добавялем контакт
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+https://ibb.co/c1mpDd6
+
+# Удаляем контакт
+
+node index.js --action remove --id=3
+
+https://ibb.co/20zdw7Q
---
+++ contacts.mjs
@@ -0,0 +1,88 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+const readContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function listContacts() {
+  try {
+    const contacts = await readContacts();
+
+    console.table(contacts);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await readContacts();
+
+    const contact = contacts.find(
+      (contact) => String(contact.id) === String(contactId)
+    );
+
+    if (!contact) throw new Error("Contact not found");
+
+    console.table(contact);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await readContacts();
+    const contact = await getContactById(contactId);
+
+    if (!contact) return;
+
+    console.log("Removing a contact...");
+
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(
+        contacts.filter((contact) => String(contact.id) !== String(contactId)),
+        null,
+        2
+      )
+    );
+
+    console.table(await readContacts());
+    console.log("The contact has been successfully deleted");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await readContacts();
+    const id = nanoid();
+    const contact = { id, name, email, phone };
+
+    console.log("Adding a contact...");
+    console.table(contact);
+
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify([...contacts, contact], null, 2)
+    );
+
+    console.table(await readContacts());
+    console.log("The contact has been successfully added");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export default { listContacts, getContactById, removeContact, addContact };
